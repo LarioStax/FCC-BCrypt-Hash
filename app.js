b0547cf4248c30e7f16f693edd5c431f79220f2a
StaxGit <lario.free@gmail.com>
2020-03-29 18:47:22 +0200

    Hash and compare passwords asynchronously

--- app.js
+++ app.js
@@ -9,12 +9,40 @@ fccTesting(app); //For FCC testing purposes
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
 const bcrypt = require("bcrypt");
 
+let hash;
 
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+	if (err) {
+		console.log(err);
+	} else {
+		hash = hash;
+		console.log("Hash 1:");
+		console.log(hash);
+		bcrypt.compare(myPlaintextPassword, hash, function (err, res) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Res 1:");
+				console.log(res);
+			}
+		});
+	}
+});
+
+
+
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+	console.log("Hash 2:");
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log("Res 2:");
+    console.log(res); //true
+  });
+});
 //END_ASYNC
 
 //START_SYNC
